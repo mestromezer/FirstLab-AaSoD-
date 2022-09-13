d9af2fceca2b0ad5befc8e0aed65e6b24eb44214
mestromezer <94231152+mestromezer@users.noreply.github.com>
2022-09-13 21:14:37 +0400

    Update Polynominal.hpp

--- Polynominal.hpp
+++ Polynominal.hpp
@@ -1,3 +1,25 @@
-class Polynominal{
-    Node* head;
+#pragma once
+
+struct Сoefficients
+{
+	double Value;
+	Сoefficients* Next;
+};
+
+class Polynominal
+{
+	Сoefficients* Values;
+	int Lenght;
+
+	int BeginnigWith;
+
+public:
+
+	Polynominal(int Power, double* Coefs, int Size);
+
+	void SetCoefs(double* Coefs);
+
+	void set(int Power, double Coef);
+
+	double operator [] (int Power);
 };
